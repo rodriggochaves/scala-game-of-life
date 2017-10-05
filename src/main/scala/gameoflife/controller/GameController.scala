2600517cc6605d642db57874d2e73f58a2549805
Kevin Souto <soutokevin@gmail.com>
2017-10-05 09:34:27 -0300

    nit

--- src/main/scala/gameoflife/controller/GameController.scala
+++ src/main/scala/gameoflife/controller/GameController.scala
@@ -5,18 +5,17 @@ import gameoflife.model.Statistics
 import gameoflife.controller.GameEngine
 
 /**
- * Relaciona o componente View com o componente Model. 
- * 
+ * Relaciona o componente View com o componente Model.
+ *
  * @author Breno Xavier (baseado na implementacao Java de rbonifacio@unb.br
  */
 object GameController {
-  
   def start {
     GameView.update
   }
-  
+
   def halt() {
-    //oops, nao muito legal fazer sysout na classe Controller
+    // oops, nao muito legal fazer sysout na classe Controller
     println("\n \n")
     Statistics.display
     System.exit(0)
@@ -24,19 +23,17 @@ object GameController {
 
   def makeCellAlive(i: Int, j: Int) {
     try {
-			GameEngine.makeCellAlive(i, j)
-			GameView.update
-		}
-		catch {
-		  case ex: IllegalArgumentException => {
-		    println(ex.getMessage)
-		  }
-		}
+      GameEngine.makeCellAlive(i, j)
+      GameView.update
+    } catch {
+      case ex: IllegalArgumentException => {
+        println(ex.getMessage)
+      }
+    }
   }
-  
+
   def nextGeneration {
     GameEngine.nextGeneration
     GameView.update
   }
-  
 }
