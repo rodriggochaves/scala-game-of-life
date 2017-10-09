aa6935ce1c69671bb7ad264d84773da066d56f61
rodriggochaves <rodriggo.chaves@gmail.com>
2017-10-09 17:33:56 -0300

    renomeando Conway -> ConwayEngine

--- src/main/scala/gameoflife/controller/ConwayEngine.scala
+++ src/main/scala/gameoflife/controller/ConwayEngine.scala
@@ -1,19 +1,13 @@
 package gameoflife.controller
 
-object Conway extends GameEngine{
-    override def shouldKeepAlive(i: Int, j: Int): Boolean = {
-
+object ConwayEngine extends GameEngine {
+  override def shouldKeepAlive(i: Int, j: Int): Boolean = {
     cells(i)(j).isAlive &&
       (numberOfNeighborhoodAliveCells(i, j) == 2 || numberOfNeighborhoodAliveCells(i, j) == 3)
-
   }
 
-
-
   override def shouldRevive(i: Int, j: Int): Boolean = {
-
     (!cells(i)(j).isAlive) &&
       (numberOfNeighborhoodAliveCells(i, j) == 3)
-
   }
 }
