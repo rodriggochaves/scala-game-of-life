b2ca332127818da45f052d4db3b257c981eb09e2
diogoq <diogoqueirozsantos97@gmail.com>
2017-10-10 08:13:44 -0300

    Adicionando um object Conway que herda de GameEngine

---
+++ src/main/scala/gameoflife/model/Conway.scala
@@ -0,0 +1,18 @@
+package gameoflife.controller
+object Conway extends GameEngine{
+    override def shouldKeepAlive(i: Int, j: Int): Boolean = {
+
+    cells(i)(j).isAlive &&
+      (numberOfNeighborhoodAliveCells(i, j) == 2 || numberOfNeighborhoodAliveCells(i, j) == 3)
+
+  }
+
+
+
+  override def shouldRevive(i: Int, j: Int): Boolean = {
+
+    (!cells(i)(j).isAlive) &&
+      (numberOfNeighborhoodAliveCells(i, j) == 3)
+
+  }
+}
