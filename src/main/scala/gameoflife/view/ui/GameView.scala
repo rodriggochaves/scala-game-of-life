80d4dd66b32643e8ef89c603f669961d38f1c35e
rodriggochaves <rodriggo.chaves@gmail.com>
2017-10-14 18:08:03 -0300

    fazendo geracao passar com um botão

--- src/main/scala/gameoflife/view/ui/GameView.scala
+++ src/main/scala/gameoflife/view/ui/GameView.scala
@@ -5,10 +5,12 @@ import scalafx.application.JFXApp.PrimaryStage
 import scalafx.Includes._
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
+import scalafx.scene.SubScene
 import scalafx.scene.control.Label
 import scalafx.scene.shape.Rectangle
 import scalafx.scene.input.MouseEvent
 import scalafx.event.EventHandler
+import scalafx.scene.control.Button
 
 import gameoflife.controller.GameEngine
 
@@ -18,9 +20,9 @@ class GameView( gameEngine: GameEngine ) extends JFXApp {
 
   stage = new PrimaryStage {
     width = 500
-    height = 500 
+    height = 500
     scene = new Scene {
-      content = initializeBoard
+      content = List(initializeBoard, controlBoard)
     }
     handleEvent(MouseEvent.Any) {
       me: MouseEvent => {
@@ -34,20 +36,37 @@ class GameView( gameEngine: GameEngine ) extends JFXApp {
     }
   }
 
-  def initializeBoard(): Array[CellRectangle] = {
+  def initializeBoard(): SubScene = {
     for(i <- (0 until gameEngine.height)) {
       for(j <- (0 until gameEngine.width)) {
         cells = cells :+ new CellRectangle( i, j, gameEngine )
       }
     }
-    return cells
+    val scene = new SubScene(300, 300)
+    scene.content = cells
+    return scene
   }
 
   def updateBoard() {
     for(i <- (0 until gameEngine.height)) {
       for(j <- (0 until gameEngine.width)) {
-        cells(i * gameEngine.width + j).isAlive(gameEngine.isCellAlive(i, j))
+        val cell = cells(i * gameEngine.width + j)
+        cell.isAlive(gameEngine.isCellAlive(i, j))
       }
     }
+  }
+
+  private def controlBoard(): SubScene = {
+    val scene = new SubScene(200, 200)
+    scene.content = new Button {
+      text = "next generation"
+      onAction = handle { 
+        gameEngine.nextGeneration 
+        updateBoard
+      }
+    }
+    scene.layoutX = 350
+    scene.layoutY = 10
+    return scene
   }
 }
