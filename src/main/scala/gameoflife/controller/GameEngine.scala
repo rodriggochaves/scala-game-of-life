e87662956df897b9e460d7cda8025c350576f740
Fillype Nascimento <fillype.n@outlook.com>
2017-10-14 19:51:11 -0300

    implementado mundo infinito utilizando método de resto

--- src/main/scala/gameoflife/controller/GameEngine.scala
+++ src/main/scala/gameoflife/controller/GameEngine.scala
@@ -9,18 +9,18 @@ import gameoflife.model.Statistics
 import gameoflife.Main
 
 /**
- * Representa a Game Engine do GoL 
- * 
+ * Representa a Game Engine do GoL
+ *
  * @author Breno Xavier (baseado na implementacao Java de rbonifacio@unb.br
  */
 abstract class GameEngine {
-  
+
   val height = Main.height
   val width = Main.width
-  
+
   val cells = Array.ofDim[Cell](height, width)
-  
-  
+
+
   for(i <- (0 until height)) {
     for(j <- (0 until width)) {
       cells(i)(j) = new Cell
@@ -31,22 +31,22 @@ abstract class GameEngine {
   /**
    * Calcula uma nova geracao do ambiente. Essa implementacao utiliza o
    * algoritmo do Conway, ou seja:
-   * 
+   *
    * a) uma celula morta com exatamente tres celulas vizinhas vivas se torna
    * uma celula viva.
-   * 
+   *
    * b) uma celula viva com duas ou tres celulas vizinhas vivas permanece
    * viva.
-   * 
+   *
    * c) em todos os outros casos a celula morre ou continua morta.
    */
-  
+
   def nextGeneration {
-    
+
     val mustRevive = new ListBuffer[Cell]
     val mustKill = new ListBuffer[Cell]
 
-    
+
     for(i <- (0 until height)) {
       for(j <- (0 until width)) {
         if(shouldRevive(i, j)) {
@@ -57,34 +57,34 @@ abstract class GameEngine {
         }
       }
     }
-    
-    
+
+
     for(cell <- mustRevive) {
       cell.revive
       Statistics.recordRevive
     }
-    
+
     for(cell <- mustKill) {
       cell.kill
       Statistics.recordKill
     }
-    
-    
+
+
   }
 
   /*
    * Verifica se uma posicao (a, b) referencia uma celula valida no tabuleiro.
    */
-  private def validPosition(i: Int, j: Int) = 
+  private def validPosition(i: Int, j: Int) =
     i >= 0 && i < height && j >= 0 && j < width;
-  
-  
+
+
   /**
    * Torna a celula de posicao (i, j) viva
-   * 
+   *
    * @param i posicao vertical da celula
    * @param j posicao horizontal da celula
-   * 
+   *
    * @throws InvalidParameterException caso a posicao (i, j) nao seja valida.
    */
   @throws(classOf[IllegalArgumentException])
@@ -96,15 +96,15 @@ abstract class GameEngine {
       throw new IllegalArgumentException
     }
   }
-  
+
   /**
    * Verifica se uma celula na posicao (i, j) estah viva.
-   * 
+   *
    * @param i Posicao vertical da celula
    * @param j Posicao horizontal da celula
    * @return Verdadeiro caso a celula de posicao (i,j) esteja viva.
-   * 
-   * @throws InvalidParameterException caso a posicao (i,j) nao seja valida. 
+   *
+   * @throws InvalidParameterException caso a posicao (i,j) nao seja valida.
    */
   @throws(classOf[IllegalArgumentException])
   def isCellAlive(i: Int, j: Int): Boolean = {
@@ -114,13 +114,13 @@ abstract class GameEngine {
       throw new IllegalArgumentException
     }
   }
-  
-  
+
+
   /**
-   * Retorna o numero de celulas vivas no ambiente. 
-   * Esse metodo eh particularmente util para o calculo de 
+   * Retorna o numero de celulas vivas no ambiente.
+   * Esse metodo eh particularmente util para o calculo de
    * estatisticas e para melhorar a testabilidade.
-   * 
+   *
    * @return  numero de celulas vivas.
    */
   def numberOfAliveCells {
@@ -131,24 +131,117 @@ abstract class GameEngine {
       }
     }
   }
-  
-  
+
+  //calcula o mod para mundo infinito em relacao as linhas
+  def returnLineMod(a : Int) : Int = {
+    var result : Int = ((a % height) + height) % height
+    return result
+  }
+
+  //calcula o mod para mundo infinito em relacao as colunas
+  def returnColumnMod(b : Int) : Int = {
+    var result : Int = ((b % width) + width) % width
+    return result
+  }
+
+  def verifyToCountCell(a : Int, b : Int, i : Int, j : Int) : Boolean = {
+    return (validPosition(a, b)  && (!(a==i && b == j)) && cells(a)(b).isAlive)
+  }
+
   // verifica se uma celula deve ser mantida viva
   def shouldKeepAlive(i: Int, j: Int) : Boolean
-  
+
   // verifica se uma celula deve (re)nascer
   def shouldRevive(i: Int, j: Int) : Boolean
-  
-  
+
+
   //  /* Computa o numero de celulas vizinhas vivas, dada uma posicao no ambiente
   //  * de referencia identificada pelos argumentos (i,j).
   //  *//
   protected def numberOfNeighborhoodAliveCells(i: Int, j: Int): Int = {
     var alive = 0
-    for(a <- (i - 1 to i + 1)) {
-      for(b <- (j - 1 to j + 1)) {
-        if (validPosition(a, b)  && (!(a==i && b == j)) && cells(a)(b).isAlive) {
-          alive += 1
+    var x = 0
+    var z = 0
+
+    if((i==0 || i==height-1) && (j>=1 && j<width-1)){
+      x = returnLineMod(i - 1)
+      for(a <- x to height-1) {
+        for(b <- (returnColumnMod(j - 1) to returnColumnMod(j + 1))) {
+          if (verifyToCountCell(a,b,i,j)) {
+            alive += 1
+          }
+        }
+      }
+
+      x = returnLineMod(i + 1)
+      for(a <- 0 to x) {
+        for(b <- (returnColumnMod(j - 1) to returnColumnMod(j + 1))) {
+          if (verifyToCountCell(a,b,i,j)) {
+            alive += 1
+          }
+        }
+      }
+
+    } else if((j==0 || j==width-1) && (i>=1 && i<height-1)){
+      x = returnColumnMod(j - 1)
+      for(a <- (returnLineMod(i - 1) to returnLineMod(i + 1))) {
+        for(b <- x to width-1) {
+          if (verifyToCountCell(a,b,i,j)) {
+            alive += 1
+          }
+        }
+      }
+
+      x = returnColumnMod(j + 1)
+      for(a <- (returnLineMod(i - 1) to returnLineMod(i + 1))) {
+        for(b <- 0 to x) {
+          if (verifyToCountCell(a,b,i,j)) {
+            alive += 1
+          }
+        }
+      }
+
+    } else if((i==0 || i==height-1) && (j==0 || j==width-1)){
+      x = returnLineMod(i - 1)
+      for(a <- x to height-1) {
+        z = returnColumnMod(j - 1)
+        for(b <- z to width-1) {
+          if (verifyToCountCell(a,b,i,j)) {
+            alive += 1
+          }
+        }
+
+        z = returnColumnMod(j + 1)
+        for(b <- 0 to z) {
+          if (verifyToCountCell(a,b,i,j)) {
+            alive += 1
+          }
+        }
+      }
+
+      x = returnLineMod(i + 1)
+      for(a <- 0 to x) {
+        z = returnColumnMod(j - 1)
+        for(b <- z to width-1) {
+          if (verifyToCountCell(a,b,i,j)) {
+            alive += 1
+          }
+        }
+
+        z = returnColumnMod(j + 1)
+        for(b <- 0 to z) {
+          if (verifyToCountCell(a,b,i,j)) {
+            alive += 1
+          }
+        }
+      }
+
+    } else{
+      for(a <- (returnLineMod(i - 1) to returnLineMod(i + 1))) {
+        for(b <- (returnColumnMod(j - 1) to returnColumnMod(j + 1))) {
+          if (verifyToCountCell(a,b,i,j)) {
+            alive += 1
+          }
         }
       }
     }
